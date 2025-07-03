5cd6a9a217056b2b4711231ba9c9ee74f15af0ba
Lisvindanu <Lisvindanu015@gmail.com>
2025-07-03 15:22:53 +0700

    update

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 // const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-const API_URL = process.env.REACT_APP_API_URL || 'https://api.vinmedia.my.id/api';
-// const API_URL = 'https://api.vinmedia.my.id/api';
+// const API_URL = process.env.REACT_APP_API_URL || 'https://api.vinmedia.my.id/api';
+const API_URL = 'https://api.vinmedia.my.id/api';
 
 // Create axios instance WITHOUT default Content-Type
 const api = axios.create({
